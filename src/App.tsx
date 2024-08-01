ab549a0fac5099b171717617003e40226adbb5f7
Matthew N <mattnanthameechai@gmail.com>
2024-08-01 16:56:17 +1200

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import type { Schema } from '../amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>()
 
@@ -21,6 +23,9 @@ function App() {
     client.models.Todo.delete({ id })
   }
   return (
+        
+    <Authenticator>
+      {({ signOut }) => (
     <main>
       <h1>Kanban</h1>
       <button onClick={createTodo}>+ new</button>
@@ -38,7 +43,11 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
+        
+      )}
+      </Authenticator>
   )
 }
 
